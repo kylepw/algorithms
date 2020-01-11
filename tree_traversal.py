acd73c684349948769f005646e235a82ac7a1a21
Kyle <kylepw@gmail.com>
2020-01-11 16:07:48 +0900

    add is_symmetric

--- tree_traversal.py
+++ tree_traversal.py
@@ -13,6 +13,8 @@ class TreeNode:
         return f'Node({self.val})'
 
 
+# DFS
+
 def inorder_traverse_re(root):
     """Inorder traversal (recursive)."""
     values = []
@@ -78,6 +80,48 @@ def postorder_traverse_iter(root):
     values.reverse()
     return values
 
+def is_symmetric_re(root: TreeNode) -> bool:
+    """Check if a binary tree is a mirror of itself (symmetric around its center)."""
+    if not root:
+        return False
+
+    def is_mirror(t1, t2):
+        if not t1 and not t2:
+            return True
+        if not t1 or not t2:
+            return False
+
+        return t1.val == t2.val and is_mirror(t1.left, t2.right) and is_mirror(t1.right, t2.left)
+
+    return is_mirror(root, root)
+
+def is_symmetric_iter(root: TreeNode) -> bool:
+    """ Check if binary tree is symmetric (iterative)."""
+    if not root:
+        return False
+
+    q = deque()
+    q.appendleft(root)
+    q.appendleft(root)
+    while q:
+        t1 = q.pop()
+        t2 = q.pop()
+
+        if not t1 and not t2:
+            continue
+        if not t1 or not t2:
+            return False
+        if t1.val != t2.val:
+            return False
+        q.appendleft(t1.left)
+        q.appendleft(t2.right)
+        q.appendleft(t1.right)
+        q.appendleft(t2.left)
+
+    return True
+
+# BTS
+
 def bts_level_order(root):
     """Print tree values by level, grouped in lists."""
     if not root:
@@ -116,6 +160,18 @@ if __name__ == '__main__':
     assert postorder_traverse_re(t) == [3, 4, 2, 1]
     assert postorder_traverse_iter(t) == [3, 4, 2, 1]
 
+    # [1,2,2,3,4,4,3]
+    e = TreeNode(1)
+    e.left = TreeNode(2)
+    e.right = TreeNode(2)
+    e.left.left = TreeNode(3)
+    e.left.right = TreeNode(4)
+    e.right.left = TreeNode(4)
+    e.right.right = TreeNode(3)
+
+    assert is_symmetric_re(e) is True
+    assert is_symmetric_iter(e) is True
+
     # BTS
     p = TreeNode(1)
     p.left = TreeNode(2)
@@ -130,3 +186,41 @@ if __name__ == '__main__':
     q.left.left.left = TreeNode(4)
     q.left.left.left.left = TreeNode(5)
     assert bts_level_order(q) == [[1], [2], [3], [4], [5]]
+
+
+    def has_path_sum(root: TreeNode, sum: int) -> bool:
+        """
+
+        Determine if the tree has a root-to-leaf path such that adding up
+        all the values along the path equals the given sum.
+
+        Ex:
+            return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
+
+        """
+        if not root:
+            return False
+
+        if not root.left and not root.right and root.val == sum:
+            return True
+
+        sum -= root.val
+
+        return has_path_sum(root.left, sum) or has_path_sum(root.right, sum)
+
+    # [5,4,8,11,null,13,4,7,2,null,null,null,1], 22
+    g = TreeNode(5)
+    g.left = TreeNode(4)
+    g.right = TreeNode(8)
+    g.left.left = TreeNode(11)
+    g.right.left = TreeNode(13)
+    g.right.right = TreeNode(4)
+
+    # [1, 2]
+    h = TreeNode(1)
+    h.left = TreeNode(2)
+
+    #print(hasPathSum(g, 22))
+    print(has_path_sum(h, 2))
+
+
